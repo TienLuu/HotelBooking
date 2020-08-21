1d2c753cd1a5480827c4595e41c9520b9a43b954
dakkhoa <dakdaoanhkhoa@gmail.com>
2020-08-21 11:39:15 +0700

    update setup

--- HotelBooking/HotelBooking/wsgi.py
+++ HotelBooking/HotelBooking/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ivivu project.
+WSGI config for HotelBooking project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ivivu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HotelBooking.settings')
 
 application = get_wsgi_application()
